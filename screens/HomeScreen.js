efc9bc8bd874226072d170dbd2e8864415f7badc
Marcus Lim <marcus4lim@gmail.com>
2019-11-30 15:55:12 +1100

    updated go

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
   Image,
   Platform,
@@ -11,22 +11,41 @@ import {
   ImageBackground,
   Animated,
 } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient'
 import { MonoText } from '../components/StyledText';
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <ImageBackground source={require('../assets/background/mount3.png')} style={{flex: 1, resizeMode: 'cover'}}>
-      <LinearGradient  colors={['#4c669f', '#3b5998', '#192f6a']}>
       <View>
       <View style={styles.profileContainer}>
         <Image source={ require('../assets/characters-and-sprites/eskimo_black.png') } style={styles.profileImage}/>
-        <Text style={styles.profileName}>NAME</Text>
-
+        <View style={styles.right_profile}>
+        <Text style={styles.profileName}>JAYDEN</Text>
+        <View style={styles.profileProgressBar}></View>
+        <Text>lvl:</Text>
+        </View>
       </View>
       </View>
-      </LinearGradient>
       </ImageBackground>
       
     </View>
@@ -73,91 +92,31 @@ const styles = StyleSheet.create({
       },
     }),
   },
+  
   profileImage: {
     'width': 80,
     'height': 80,
     marginRight: 20,
   },
-  profileName: {
-    'flex': 2,
-    fontFamily: 'monospace',
-    fontSize: 25,
+  right_profile: {
     marginTop: 5,
+  },
+  profileName: {
+    fontSize: 25,
     fontWeight: 'bold',
   },
   contentContainer: {
     paddingTop: 30,
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
+  profileProgressBar: {
+     height: 10,
+     width: '100%',
+     backgroundColor: 'white',
+     borderColor: '#000',
+     borderWidth: 2,
+     borderRadius: 5
+   },
   navigationFilename: {
     marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
