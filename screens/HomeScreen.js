3124988f21dd2cd61171f36997d0d4091aaff1ce
Marcus Lim <marcus4lim@gmail.com>
2019-11-30 23:35:01 +1100

    GOGOGO

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import * as WebBrowser from 'expo-web-browser';
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, Component } from 'react';
 import {AsyncStorage} from 'react-native';
 import { withNavigation } from 'react-navigation';
 import {
@@ -35,150 +35,143 @@ function useInterval(callback, delay) {
 }
 
 class HomeScreen extends Component {
-  componentDidMount(){
-      // Toggle the state every second
-      setInterval(() => (
-        this.setState(previousState => (
-          { isShowingText: !previousState.isShowingText }
-        ))
-      ), 1000);
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress:0,
+    };
   }
-  state = { isShowingText: true };
-  let animation = useRef(new Animated.Value(0));
-  const [progress, setProgress] = useState(0);
-   useInterval(() => {
-     if(progress < 70 ) {
-       setProgress(progress + 5);
-     }
-   }, 100);
-  useEffect(() => {
-     Animated.timing(animation.current, {
-       toValue: progress,
-       duration: 100
-     }).start();
-   },[progress]);
-  const widthBAR = animation.current.interpolate({
-    inputRange: [0, 100],
-    outputRange: ["0%", "100%"],
-    extrapolate: "clamp"
-  });
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={require('../assets/background/clouds.gif')} style={{flex: 1, resizeMode: 'cover'}}>
-      <View style={{backgroundColor: '#7481FCFF'}}></View>
-      <ImageBackground source={require('../assets/background/backgroundmountain.png')} style={{flex: 1, resizeMode: 'cover'}}>
-      <View style={styles.profileContainer}>
-        <Image source={ require('../assets/characters-and-sprites/eskimo_black.png') } style={styles.profileImage}/>
-        <View style={styles.right_profile}>
-        <Text style={styles.profileName}>JAYDEN</Text>
-        <View style={styles.profileProgressBar}>
-          <Animated.View style={{position: 'absolute',
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            backgroundColor: "#8BED4F", width: widthBAR,}}/>
+  componentDidUpdate(){
+      // Toggle the state every second
+      useInterval(() => {
+           if(this.state.progress < 70 ) {
+             setProgress(this.state.progress + 5);
+           }
+         }, 100);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={require('../assets/background/clouds.gif')} style={{flex: 1, resizeMode: 'cover'}}>
+        <View style={{backgroundColor: '#7481FCFF'}}></View>
+        <ImageBackground source={require('../assets/background/backgroundmountain.png')} style={{flex: 1, resizeMode: 'cover'}}>
+        <View style={styles.profileContainer}>
+          <Image source={ require('../assets/characters-and-sprites/eskimo_black.png') } style={styles.profileImage}/>
+          <View style={styles.right_profile}>
+          <Text style={styles.profileName}>JAYDEN</Text>
+          <View style={styles.profileProgressBar}>
+            <Animated.View style={{position: 'absolute',
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              backgroundColor: "#8BED4F", width: this.state.progress,}}/>
+          </View>
+          <Text>Progress:{`${this.state.progress}%`}</Text>
+          <Text>{100 - `${this.state.progress}%`} till next audio</Text>
+          </View>
         </View>
-        <Text>Progress:{`${progress}%`}</Text>
-        <Text>{100 - `${progress}%`} till next audio</Text>
+        <View style={{display:'flex', height:'100%'}}>
+          <View style={{flex:1}}></View>{/*Spacer for mountain*/}
+          <View style={{flex:3, }}>
+             <View style={{flex:2, }}>
+               <View style={{
+                transform: [
+                  {rotate: '240deg'}
+                ],
+                height: 10,
+                left: '52%',
+                top: '35%',
+                width: '26%',
+                backgroundColor: 'white',
+                borderRadius: 5
+               }}>
+                 <Animated.View style={{position: 'absolute',
+                   left: 0,
+                   right: 0,
+                   top: 0,
+                   bottom: 0,
+                   backgroundColor: "#8BED4F", width: 0,}}/>
+               </View>
+             </View>
+             <View style={{flex:3}}>
+               <View style={{
+                transform: [
+                  {rotate: '320deg'}
+                ],
+                height: 10,
+                left: '29%',
+                top: '44%',
+                width: '48%',
+                backgroundColor: 'white',
+                borderRadius: 5
+               }}>
+                 <Animated.View style={{position: 'absolute',
+                   left: 0,
+                   right: 0,
+                   top: 0,
+                   bottom: 0,
+                   backgroundColor: "#8BED4F", width: this.state.progress,}}/>
+               </View>
+             </View>
+             <View style={{flex:5, }}>
+              <View style={{
+                transform: [
+                  {rotate: '220deg'}
+                ],
+                height: 10,
+                left: '28%',
+                width: '50%',
+                top: '34%',
+                backgroundColor: 'white',
+                borderRadius: 5
+               }}>
+                 <Animated.View style={{position: 'absolute',
+                   left: 0,
+                   right: 0,
+                   top: 0,
+                   bottom: 0,
+                   backgroundColor: "#8BED4F", width: '100%',}}/>
+               </View>
+             </View>
+             <View style={{flex:5, }}>
+              <View style={{
+                transform: [
+                  {rotate: '320deg'}
+                ],
+                height: 10,
+                left: '6%',
+                width: '75%',
+                top: '18%',
+                backgroundColor: 'white',
+                borderRadius: 5
+               }}>
+                 <Animated.View style={{position: 'absolute',
+                   left: 0,
+                   right: 0,
+                   top: 0,
+                   bottom: 0,
+                   backgroundColor: "#8BED4F", width: '100%',}}/>
+               </View>
+             </View>
+           </View>
         </View>
+        </ImageBackground>
+        </ImageBackground>
       </View>
-      <View style={{display:'flex', height:'100%'}}>
-        <View style={{flex:1}}></View>{/*Spacer for mountain*/}
-        <View style={{flex:3, }}>
-           <View style={{flex:2, }}>
-             <View style={{
-              transform: [
-                {rotate: '240deg'}
-              ],
-              height: 10,
-              left: '52%',
-              top: '35%',
-              width: '26%',
-              backgroundColor: 'white',
-              borderRadius: 5
-             }}>
-               <Animated.View style={{position: 'absolute',
-                 left: 0,
-                 right: 0,
-                 top: 0,
-                 bottom: 0,
-                 backgroundColor: "#8BED4F", width: 0,}}/>
-             </View>
-           </View>
-           <View style={{flex:3}}>
-             <View style={{
-              transform: [
-                {rotate: '320deg'}
-              ],
-              height: 10,
-              left: '29%',
-              top: '44%',
-              width: '48%',
-              backgroundColor: 'white',
-              borderRadius: 5
-             }}>
-               <Animated.View style={{position: 'absolute',
-                 left: 0,
-                 right: 0,
-                 top: 0,
-                 bottom: 0,
-                 backgroundColor: "#8BED4F", width: widthBAR,}}/>
-             </View>
-           </View>
-           <View style={{flex:5, }}>
-            <View style={{
-              transform: [
-                {rotate: '220deg'}
-              ],
-              height: 10,
-              left: '28%',
-              width: '50%',
-              top: '34%',
-              backgroundColor: 'white',
-              borderRadius: 5
-             }}>
-               <Animated.View style={{position: 'absolute',
-                 left: 0,
-                 right: 0,
-                 top: 0,
-                 bottom: 0,
-                 backgroundColor: "#8BED4F", width: '100%',}}/>
-             </View>
-           </View>
-           <View style={{flex:5, }}>
-            <View style={{
-              transform: [
-                {rotate: '320deg'}
-              ],
-              height: 10,
-              left: '6%',
-              width: '75%',
-              top: '18%',
-              backgroundColor: 'white',
-              borderRadius: 5
-             }}>
-               <Animated.View style={{position: 'absolute',
-                 left: 0,
-                 right: 0,
-                 top: 0,
-                 bottom: 0,
-                 backgroundColor: "#8BED4F", width: '100%',}}/>
-             </View>
-           </View>
-         </View>
-      </View>
-      </ImageBackground>
-      </ImageBackground>
-    </View>
   );
+  }
 }
+export default HomeScreen;
 
 HomeScreen.navigationOptions = {
   header: null,
 };
+
+
 function getUserIcon() {
 }
+
 
 function handleLearnMorePress() {
   WebBrowser.openBrowserAsync(
